665128eec33e6b896c822bf38d1058d4b9b47b3b
Arthur kunz <arthurhkunz@gmail.com>
2024-03-22 17:59:24 -0300

    feat: supabase key changed

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabase = createClient(
-  import.meta.env.VITE_SUPABASE_URL as string,
-  import.meta.env.VITE_SUPABASE_ANON_KEY as string
+  "https://rtiuypxlbwtnhowcjcdq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJ0aXV5cHhsYnd0bmhvd2NqY2RxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkwNjk1MzIsImV4cCI6MjAyNDY0NTUzMn0.N_VrB0bOAT2ASP12hKYEiXDg6LrX7DpHMoi-t_udPf4"
 );
 
 export default supabase;
